6245d1b602df777616aad28714587d5f2f75f49c
Hunter <2nd1rst@gmail.com>
2023-02-09 11:19:01 -0500

    1

--- dwmbar.sh
+++ dwmbar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-	xsetroot -name "$(date +'%l:%M') $(free -h | awk /Mem:/'{print "RAM:"$3}') E:$(cat /sys/class/net/enp0s3/operstate)"
+	xsetroot -name "$(date +'%l:%M') | $(df -h / | awk '/[0-9]/{print $3 "/" $2}') | $(free -h | awk /Mem:/'{print "RAM:"$3}') | E:$(cat /sys/class/net/enp0s3/operstate)"
 	sleep 5 
 done
